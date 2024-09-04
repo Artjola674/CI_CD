ff8353d20c569935743948d341db102c8ca42608
Artjola674 <66790977+Artjola674@users.noreply.github.com>
2024-09-04 11:58:51 +0200

    Update CiCdApplicationTests.java

--- src/test/java/com/example/CI_CD/CiCdApplicationTests.java
+++ src/test/java/com/example/CI_CD/CiCdApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.CI_CD;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 @SpringBootTest
 class CiCdApplicationTests {
